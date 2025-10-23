c00568f77245e2800980a330246c413f8235ba0c
Gzc1217 <3208074303@qq.com>
2025-10-23 20:24:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def PrimeList(N):
     参数:     N - 正整数
     返回:     str - 包含所有小于 N 的质数的字符串
     """
-    primes = []
+    primes = int(input())
     for num in range(1, N):
         is_prime = True
         for i in range(1, int(num ** 0.5) + 1):
